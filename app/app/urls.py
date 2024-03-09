faf1ccbd69d65e9d9803fa24cea0588f3ca28564
Joel Emmanuel <ejoel0035@gmail.com>
2024-03-09 09:20:25 +0100

    feat: set api docs

--- app/app/urls.py
+++ app/app/urls.py
@@ -5,7 +5,6 @@ from drf_spectacular.views import (
 
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
