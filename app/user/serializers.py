ba88d43d406927f173189cebb3cc6bf8fcf974c4
Joel Emmanuel <ejoel0035@gmail.com>
2024-03-09 09:29:23 +0100

    fix: trim white space

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -40,7 +40,7 @@ class AuthTokenSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(
         style={'input_type': 'password'},
-        trim_whitespace=True
+        trim_whitespace=False
     )
 
     def validate(self, attrs):
